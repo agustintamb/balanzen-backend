fe9a518787800ba62099648a93e216428f7e2c62
Agustin Tamburrino <agustingonzaleztam@gmail.com>
2026-04-29 18:47:19 -0300

    Fix swagger config servers

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -1,20 +1,13 @@
 import swaggerJsdoc from "swagger-jsdoc";
+import envConfig from "./env.config.js";
 
 const options = {
   definition: {
     openapi: "3.0.0",
     servers: [
       {
-        url: "http://localhost:3001/api/v1",
-        description: "Servidor de desarrollo",
-      },
-      {
-        url: "https://balanzen-backend-testing.up.railway.app/api/v1",
-        description: "Servidor de pruebas",
-      },
-      {
-        url: "https://balanzen-backend-production.up.railway.app/api/v1",
-        description: "Servidor de producción",
+        url: `${envConfig.baseUrl}/api/v1`,
+        description: `Servidor (${envConfig.env})`,
       },
     ],
     info: {
